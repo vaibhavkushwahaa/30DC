8f699077d9f82fbb63f40610c38c91a882f85499
[Vaibhav Kushwaha] <[vaibhavkushwaha1973@gmail.com]>
2024-04-05 18:57:11 +0530

    update js

---
+++ JS/datatype.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version of JS
+
+// alert(3+3) we are using node js not browser 
+
+console.log(3+3)
+console.log("Vaibhav")
+
+let name="vaibhav"
+let age = 32
+let isloggeedin = true
+let state= null
+
+// number, string, boolean, undefined, null, object, symbol,bigint
+console.log(typeof null)
+
---
+++ JS/variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountEmail = "vaibhavkushwaha1973@gmail.com"
+var accountPassword = "password123"
+accountcity = "Lucknow"
+let accountstate
+
+// accountId = 54321 //not allowed
+accountEmail ="vaibhav9824@gmail.com"
+accountPassword = "password@123"
+accountcity = "Delhi"
+
+/*
+prefer not to use var 
+beacuse of issue in block and function scoped
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountcity,accountstate])
